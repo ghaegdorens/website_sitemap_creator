3fbe811e6030e277b15cbddea6c36da4d9753fe8
Gert Haegdorens <gert@300solutions.com>
2019-08-24 07:30:58 -0500

    Change href list and cleanup (#2)
    * Find all href and print them
    
    * DIscovery code with output
    
    * Print all atrributes
    
    * Remote output file
    
    * Change url to 300solutions.com

--- files/app/start.py
+++ files/app/start.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 
 
-def main():
-    page = requests.get('https://www.lifebankusa.com/sitemap')
+def main()
+    page = requests.get('https://www.300solutions.com/')
 
     # Create a BeautifulSoup object
     soup = BeautifulSoup(page.text, 'html.parser')
